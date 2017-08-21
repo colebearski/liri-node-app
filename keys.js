e36d8217c89f81899a6af5d78d423f8d3abc4256
colebearski <cole.pedersen@gmail.com>
2017-08-21 17:16:57 -0600

    twitter goodies

--- keys.js
+++ keys.js
@@ -1,8 +1,10 @@
 console.log('this is loaded');
 
+// twitter keys
+
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
+  consumer_key: 'BNJlBTCnmDVbvbQrrYVGm2yVG',
+  consumer_secret: 'l2gsA7ZySAmppr9ebpMk05R6dq39XTPchugw4k5XvbrToinZYh',
   access_token_key: '<input here>',
   access_token_secret: '<input here>',
 };
