818814a8e8e5aaa4d962091e4c18a78b1f14e2d5
colebearski <cole.pedersen@gmail.com>
2017-08-21 17:21:51 -0600

    liri js dog

---
+++ liri.js
@@ -0,0 +1,14 @@
+// add request here for keys js
+// store the following in variables
+// or just do the following below:
+
+/*
+At the top of the liri.js file, write the code you need to grab the data from keys.js. 
+Then store the keys in a variable.
+Make it so liri.js can take in one of the following commands:
+
+my-tweets
+spotify-this-song
+movie-this
+do-what-it-says
+*/
